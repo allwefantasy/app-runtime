6c1f41894c70791a968e1e519dfcfcf3624d2a35
WilliamZhu <allwefantasy@gmail.com>
2020-01-29 20:27:25 +0800

    udpate

--- src/main/java/tech/mlsql/serviceframework/platform/action/RegisterPluginAction.scala
+++ src/main/java/tech/mlsql/serviceframework/platform/action/RegisterPluginAction.scala
@@ -1,13 +1,24 @@
 package tech.mlsql.serviceframework.platform.action
 
+import tech.mlsql.common.utils.log.Logging
 import tech.mlsql.common.utils.serder.json.JSONTool
-import tech.mlsql.serviceframework.platform.PluginLoader
+import tech.mlsql.serviceframework.platform.app.AppManager
+import tech.mlsql.serviceframework.platform.{AppRuntimeStore, PluginLoader, PluginType}
 
-class RegisterPluginAction extends CustomAction {
+class RegisterPluginAction extends CustomAction with Logging {
   override def run(params: Map[String, String]): String = {
     val url = params("url")
     val className = params("className")
     val loader = PluginLoader.load(url, className)
+    val plugin = loader.plugin
+    plugin.entries.foreach { pi =>
+      if (pi.pluginType == PluginType.app) {
+        AppRuntimeStore.store.getApp(pi.name).foreach { appItem =>
+          logInfo(s"Plugin: load plugin ${appItem.name}")
+          AppManager.call(appItem.name, Map())
+        }
+      }
+    }
     JSONTool.toJsonStr(List())
   }
 }
